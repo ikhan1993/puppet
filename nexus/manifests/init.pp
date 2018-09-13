dac29f4790ebe14dc73e53c3e473e5e6371ddc11
root <root@masterMachineDG.qac.local>
2018-09-13 15:57:04 +0000

    update

--- nexus/manifests/init.pp
+++ nexus/manifests/init.pp
@@ -1,4 +1,7 @@
 class nexus {
+  Exec { cwd => '/home/vagrant' }
+  Exec { path => ['/usr/bin', '/usr/sbin', '/etc/init.d'] }
+
   package { 'java':
     ensure => installed,
   }
@@ -12,44 +15,38 @@ class nexus {
   }
 
   exec { 'get-nexus':
-    cwd => '/home/vagrant',
     command => 'sudo wget https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.0.2-02-unix.tar.gz',
-    path => ['/usr/bin', '/usr/sbin',],
   }
 
   exec { 'unzip-nexus':
-    cwd => '/home/vagrant',
-    path => ['/usr/bin', '/usr/sbin',],
-    command => 'sudo tar -xvf nexus-3.0.2-02-unix.tar.gz,
+    command => 'sudo tar -xvf nexus-3.0.2-02-unix.tar.gz',
   }
 
   exec { 'move-nexus':
-    cwd => '/home/vagrant',
-    path => ['/usr/bin', '/usr/sbin',],
     command => 'sudo mv nexus-3.0.2-02 nexus'
   }
 
-  exec { 'adding-nexus-user':
-    cwd => '/home/vagrant',
-    path => ['/usr/bin', '/usr/sbin',],
-    command => 'sudo adduser nexus',
+  user { 'adding-nexus-user':
+    name => 'nexus',
+    ensure => 'present',
   }
 
-  exec { 'run-user':
-    cwd => '/home/vagrant',
-    path => ['/usr/bin', '/usr/sbin',],
-    command => 'sudo run_as_user="nexus"',
+  exec { 'run nexus':
+    cwd     => '/home/vagrant/nexus/bin/',
+    command => 'bash nexus run',
   }
 
-  exec { 'put-nexus-in-init.d':
-    cwd => '/home/vagrant',
-    path => ['/usr/bin', '/usr/sbin',],
-    command => 'ln -s nexus/bin/nexus /etc/init.d/nexus'
-  }
+  #exec { 'run-user':
+  #  command => 'sudo run_as_user="nexus"',
+  #}
 
-  exec { 'running-nexus':
-    cwd => '/home/vagrant',
-    path => ['/usr/bin', '/usr/sbin',],
-    command => './nexus/bin/nexus run',
-  }
+  #exec { 'put-nexus-in-init.d':
+   # command => 'mv nexus/bin/nexus /etc/init.d/'
+  #}
+
+  #exec { 'run-nexus':
+   # user => 'nexus',
+   # cwd => '/etc/init.d',
+   # command => 'bash nexus run',
+  #}
 }
